9ea284f097960a1689bdfdd847995be299b64454
musab <musab.jolani@gmail.com>
2024-11-24 23:51:56 +0200

    UserPage ...

--- client/src/UsersPage.jsx
+++ client/src/UsersPage.jsx
@@ -1,29 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import User from './User';
 
 function UsersPage() {
-  const [count, setCount] = useState(0);
+  
   const usersPageStyle=
 {
   "background-color": "yellowgreen",
   "border-radius": "20px",
   "border":"1.5px solid grey",
   "height": "400px",
-  "width": "250px",
-  "padding": "0.5rem"
+  "width": "270px",
+  "padding": "0.5rem",
+  "overflow": "scroll"
 }
 
   return (
-    <>
-     <div  style={usersPageStyle}>
-        <label>Search</label>
-        <input style={{}}size="" type="text"/>
-        <button style={{float:"right"}}>Add</button>
-     
-    </div>
-    </>
+  <div  style={usersPageStyle}>
+    <label>Search</label>
+    <input style={{}}size="" type="text"/>
+    <button style={{float:"right"}}>Add</button>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+    <User></User>
+  
+  </div>
+  
   )
 }
 
